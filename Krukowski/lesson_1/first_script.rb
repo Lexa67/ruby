08feea765c238696b99d33e3b5b1ef93e980fe23
Lexa <3aj4ik504@gmail.com>
2023-05-01 23:40:20 +0300

    01.05.2023_22:30

---
+++ Krukowski/lesson_1/first_script.rb
@@ -0,0 +1,19 @@
+#1 mile = 1.6 km
+
+user_input_in_miles = gets.to_f
+
+
+result_in_kilometers = user_input_in_miles * 1.6
+
+puts "Значение в километрах: #{result_in_kilometers}"
+
+
+
+
+
+
+puts "Введите значение в километрах"
+usr_1 = gets.to_f
+user_2 = usr_1 / 1.6
+
+puts "Значение в милях: #{user_2}!"
