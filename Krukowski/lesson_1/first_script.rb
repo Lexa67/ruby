00580955fc3badb363847a6a497c1a77419cedc6
Lexa <3aj4ik504@gmail.com>
2023-05-01 23:42:15 +0300

    Ruby с нуля, урок #1 | Настройка, переменные, типы данных

--- Krukowski/lesson_1/first_script.rb
+++ Krukowski/lesson_1/first_script.rb
@@ -7,11 +7,6 @@ result_in_kilometers = user_input_in_miles * 1.6
 
 puts "Значение в километрах: #{result_in_kilometers}"
 
-
-
-
-
-
 puts "Введите значение в километрах"
 usr_1 = gets.to_f
 user_2 = usr_1 / 1.6
