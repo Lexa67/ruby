abe10388588ecc627d847a775e4be49edafd1976
Lexa67 <3aj4ik504@gmail.com>
2023-05-11 18:14:01 +0300

    11.05.2023_17:00

--- Programist/lesson_6/array1.rb
+++ Programist/lesson_6/array1.rb
@@ -5,9 +5,9 @@ user = gets.chomp.to_i
 loop do
   if user > 0
     print "Нужно просуммировать #{user} "
-    if user == 1 || (user % 10) == 1 && user > 20
+    if user == 1 || ((user % 10) == 1 && user > 20)
       puts "число"
-    elsif user > 1 && user < 5 || (user % 10) > 1 && (user % 10) < 5 && user > 20
+    elsif (user > 1 && user < 5) || ((user % 10) > 1 && (user % 10) < 5 && user > 20)
       puts "числа"
     else
       puts "чисел"
